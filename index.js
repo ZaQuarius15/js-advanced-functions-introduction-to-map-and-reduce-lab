d57b12a9f033cc506205da090e70274a95fa7785
ZaQuarius15 <jzcarlson014@gmail.com>
2020-11-04 19:36:11 -0600

    commit tree 6cf33db8dabc436311d274bbbae9f09a5e9d1632

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
-// Your code here
+function mapToNegativize(array) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++ ) {
+        newArray.push(-1 * array[i])
+    }
+    return newArray
+}
+
+function mapToNoChange(array) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(array[i])
+    }
+    return newArray
+}
+
+function mapToDouble(array) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(array[i] * 2)
+    }
+    return newArray
+}
+
+function mapToSquare(array) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(array[i] ** 2)
+    }
+    return newArray
+}
+
+function reduceToTotal(arr, start=0) {
+    let total = start
+    for (let i = 0; i < arr.length; i++) {
+        total += arr[i]
+    }
+    return total
+}
+
+function reduceToAllTrue (arr) {
+    let bool = true
+    for (let i = 0; i < arr.length; i++) {
+        if (!!arr[i] === true) {
+            bool = true
+        } else {
+            bool = false
+        }
+    }
+    return bool
+}
+
+function reduceToAnyTrue (arr) {
+    let bool = false
+    for (let i = 0; i < arr.length; i++) {
+        if (!!arr[i] === true) {
+            bool = true
+        }
+    }
+    return bool
+}
